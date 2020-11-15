379520168a53c1bf0d2c8230f90c945383b181de
reap3r97 <40631824+reap3r97@users.noreply.github.com>
2020-11-15 15:33:12 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 910) {
+            $('.navbar').addClass('fixed-top');
+        }
+        if ($(window).scrollTop() < 910) {
+            $('.navbar').removeClass('fixed-top');
+        }
+    });
+});
+
+$(".anim ul li a[href^='#']").on('click', function (e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 1000, function () {
+        window.location.hash = hash;
+    });
+});
+
+
